259dfbb07c8e47cd92a6ddb6d10933932044d4c5
pascal588 <pascalbima973@gmail.com>
2025-08-26 16:30:41 +0700

    api divisions

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 // routes/api.php
-use App\Http\Controllers\DivisionController;
+use App\Http\Controllers\Api\DivisionController;
 
 // Gunakan full namespace dengan method array
-Route::apiResource('/Division', DivisionController::class);
-Route::get('/Division', [DivisionController::class, 'index']);
-
-// Atau gunakan string syntax
-Route::apiResource('division', 'App\Http\Controllers\Api\DivisionController');
+Route::apiResource('Division', DivisionController::class);
 
